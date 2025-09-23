786d417bec9d5cbf88d84f4936ff352a3ef6c1d1
Jideotetic <jideotetic@gmail.com>
2025-09-23 19:52:59 +0100

    Completed footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,78 @@
+import { NAV_LINKS } from "@/lib/constants";
 import React from "react";
+import { Link } from "react-router";
+import { RiInstagramFill, RiTiktokFill, RiTwitterXFill } from "react-icons/ri";
+import { FaFacebook } from "react-icons/fa";
 
 function Footer() {
 	return (
-		<footer className="mx-auto text-[#F3D225] md:text-[18px] flex flex-col md:flex-row gap-2 w-full max-w-[1120px] items-center justify-between">
-			<span>BOOM — the token that goes brrr.</span>
+		<footer className="text-[#0D0106] md:text-[18px] font-light relative">
+			<div className="px-5 py-10 pb-30 sm:pb-22">
+				<div className="w-full mx-auto max-w-[1120px] flex flex-col sm:flex-row gap-6 sm:justify-between flex-wrap">
+					<div className="space-y-5 max-w-[320px] md:max-w-[350px]">
+						<div className="flex flex-col gap-3">
+							<span className="text-[24px] font-extrabold text-[#FE7A04]">
+								AFRO.A
+							</span>
+							<span>
+								For creators who may not be known today, but will shape the
+								culture tomorrow.
+							</span>
+						</div>
 
-			<span>© 2025 BoomSquad. All rights reserved </span>
+						<div className="flex flex-col gap-2 font-normal">
+							<span>afroascend@gmail.com</span>
+							<span>+12384652670</span>
+						</div>
+					</div>
+					<div className="space-y-3">
+						<div className="text-[18px] font-bold text-[#FE7A04]">Explore</div>
+
+						<ul className="flex flex-col gap-3">
+							{NAV_LINKS.map((link) => (
+								<li key={link.title}>
+									<Link
+										className="hover:text-[#FE7A04] hover:underline"
+										to={link.href}
+									>
+										{link.title}
+									</Link>
+								</li>
+							))}
+						</ul>
+					</div>
+					<div className="space-y-3">
+						<div className="text-[18px] font-bold text-[#FE7A04]">
+							Social Links
+						</div>
+
+						<ul className="space-y-3">
+							<li className="group flex hover:text-[#FE7A04] items-center gap-2">
+								<RiTwitterXFill className="rounded-[4px] group-hover:text-[#FE7A04] text-[20px] text-[#0D0106]" />
+								<a href="">Twitter</a>
+							</li>
+							<li className="group hover:text-[#FE7A04] flex items-center gap-2">
+								<RiInstagramFill className="rounded-[4px] group-hover:text-[#FE7A04] text-[20px] text-[#0D0106]" />
+								<a href="">Instagram</a>
+							</li>
+							<li className="group hover:text-[#FE7A04] flex items-center gap-2">
+								<RiTiktokFill className="rounded-[4px] group-hover:text-[#FE7A04] text-[20px] text-[#0D0106]" />
+								<a href="">TikTok</a>
+							</li>
+							<li className="group hover:text-[#FE7A04] flex items-center gap-2">
+								<FaFacebook className="rounded-[4px] group-hover:text-[#FE7A04] text-[20px] text-[#0D0106]" />
+								<a href="">Facebook</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+
+			<div className="absolute bottom-0 left-0 right-0 bg-[url(/Group-30.svg)] bg-cover bg-center bg-no-repeat px-5 py-10">
+				<div className="mx-auto w-full max-w-[1120px] sm:text-center">
+					©️ 2025 Afro Ascend. All rights reserved.
+				</div>
+			</div>
 		</footer>
 	);
 }
--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -5,7 +5,7 @@ import Footer from "./Footer";
 function RootLayout() {
 	return (
 		<>
-			<div className="fixed right-0 left-0 z-50 bg-[url(/Group-30.svg)] bg-cover bg-center bg-no-repeat px-5 py-4">
+			<div className="fixed bg-[#FFFCF2] right-0 left-0 z-50 bg-[url(/Group-30.svg)] bg-cover bg-center bg-no-repeat px-5 py-4">
 				<Header />
 			</div>
 
@@ -13,7 +13,7 @@ function RootLayout() {
 				<Outlet />
 			</main>
 
-			<div className="bg-[#0B0F15] px-5 py-10">
+			<div className="bg-[#FFFCF2]">
 				<Footer />
 			</div>
 		</>
